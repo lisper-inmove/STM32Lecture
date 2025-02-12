9847f993cabdb3b95ff99a7d6e89b7baf90f27e6
inmove <lisper.inmove@gmail.com>
2025-02-12 21:38:30 +0800

    Lession10

--- Hw/Src/led.c
+++ Hw/Src/led.c
@@ -5,7 +5,7 @@ void LED_Init(void) {
 	GPIO_InitTypeDef GPIO_InitType;
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 	GPIO_InitType.Pin = GPIO_PIN_0;
-	GPIO_InitType.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitType.Mode = GPIO_MODE_OUTPUT_OD;
 	GPIO_InitType.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitType);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
