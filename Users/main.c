d4d1a167e1b26b4dfd7d0f85cd0676c7e38ecd8a
inmove <lisper.inmove@gmail.com>
2025-02-15 14:20:27 +0800

    Lession16

--- Users/main.c
+++ Users/main.c
@@ -9,8 +9,13 @@ int main(void) {
 	// HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_SYSCLK, RCC_MCODIV_1);
 	LED_Init();
 	SW_Init_IT(1);
+	HAL_Delay(5000);
+	// 将GPIO的引脚锁定，直到下次复位，再也无法修改
+	HAL_GPIO_LockPin(GPIOB, GPIO_PIN_0);
+	// 将GPIO引脚的初始化
+	HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0);
 	while (1) {
-		HAL_Delay(1000);
-		__HAL_GPIO_EXTI_GENERATE_SWIT(GPIO_PIN_13);
+		// HAL_Delay(1000);
+		// __HAL_GPIO_EXTI_GENERATE_SWIT(GPIO_PIN_13);
 	}
 }
