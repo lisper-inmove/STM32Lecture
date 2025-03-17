d740dfdb9356fc98b3d3c6035f7f260f6587bf9b
inmove <lisper.inmove@gmail.com>
2025-03-17 22:03:05 +0800

    外触发模式: 5次边沿触发定时器

--- Hw/Src/timer.c
+++ Hw/Src/timer.c
@@ -4,8 +4,7 @@
 #include "util.h"
 
 TIM_HandleTypeDef timer1;
-DMA_HandleTypeDef timer1_dmaup;
-uint16_t timer1_dmabuff[4] = {3999, 5999, 7999, 9999};
+TIM_ClockConfigTypeDef timer1_clock;
 uint32_t counter = 0;
 
 void Timer1_Init(uint16_t arr, uint16_t psc, uint8_t rep) {
@@ -16,26 +15,23 @@ void Timer1_Init(uint16_t arr, uint16_t psc, uint8_t rep) {
   timer1.Init.RepetitionCounter = rep;
   timer1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   HAL_TIM_Base_Init(&timer1);
-  HAL_TIM_Base_Start_DMA(&timer1, (uint32_t *)timer1_dmabuff, 4);
+	__HAL_TIM_CLEAR_FLAG(&timer1, TIM_FLAG_UPDATE);
+	
+	timer1_clock.ClockSource = TIM_CLOCKSOURCE_TI1ED;
+	HAL_TIM_ConfigClockSource(&timer1, &timer1_clock);
+	
+  HAL_TIM_Base_Start(&timer1);
 }
 
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim) {
+	GPIO_InitTypeDef GPIO_InitType;
   if (htim->Instance == TIM1) {
     __HAL_RCC_TIM1_CLK_ENABLE();
-    __HAL_RCC_DMA1_CLK_ENABLE();
-
-    timer1_dmaup.Instance = DMA1_Channel5;
-    timer1_dmaup.Init.Direction = DMA_MEMORY_TO_PERIPH;
-    timer1_dmaup.Init.PeriphInc = DMA_PINC_DISABLE;
-    timer1_dmaup.Init.MemInc = DMA_MINC_ENABLE;
-    timer1_dmaup.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
-    timer1_dmaup.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-    timer1_dmaup.Init.Mode = DMA_NORMAL;
-    timer1_dmaup.Init.Priority = DMA_PRIORITY_MEDIUM;
-    __HAL_LINKDMA(&timer1, hdma[TIM_DMA_ID_UPDATE], timer1_dmaup);
-    HAL_DMA_Init(&timer1_dmaup);
-
-    HAL_NVIC_SetPriority(DMA1_Channel5_IRQn,3,0);
-    HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		
+		GPIO_InitType.Pin = GPIO_PIN_8;
+		GPIO_InitType.Mode = GPIO_MODE_INPUT;
+		GPIO_InitType.Pull = GPIO_PULLDOWN;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitType);
   }
 }
--- Users/main.c
+++ Users/main.c
@@ -1,11 +1,27 @@
 #include "stm32f1xx_hal.h"
 #include "rcc.h"
 #include "util.h"
+#include "timer.h"
 
 int main(void) {
   HAL_Init();
   RccClock_Init();
-  UART1_Init(921600);
-  u1_printf("Program start: %d\n", 10);
-  while (1) {}
+  UART2_Init(921600);
+	Timer1_Init(4, 0, 0);
+	// RX 连接 PA2
+  u2_printf("Program start: %d\n", 10);
+	
+	// PA8 连接 SW8
+	// PA9 连接 SW11
+	// PA12 连接 SW12
+  while (1) {
+		if (__HAL_TIM_GET_FLAG(&timer1, TIM_FLAG_UPDATE)) {
+			__HAL_TIM_CLEAR_FLAG(&timer1, TIM_FLAG_UPDATE);
+			u2_printf("Timer1 Update.");
+		}
+		if (__HAL_TIM_GET_FLAG(&timer1, TIM_FLAG_TRIGGER)) {
+			__HAL_TIM_CLEAR_FLAG(&timer1, TIM_FLAG_TRIGGER);
+			u2_printf("Timer1 Trigger.");
+		}
+	}
 }
